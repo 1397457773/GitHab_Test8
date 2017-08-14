0a642662284b5db81906b517db557f9268c004bc
1397457773 <1397457773@qq.com>
2017-08-14 23:45:43 +0800

    第四次提交 框架内容填充，新建方法
    时间：2017年8月14日23:45:29
    程序员：李俊岐

--- app/src/main/java/com/jiyun/githab_test3/Demo.java
+++ app/src/main/java/com/jiyun/githab_test3/Demo.java
@@ -1,5 +1,6 @@
 package com.jiyun.githab_test3;
 
+import android.provider.Settings;
 import android.util.Log;
 
 /**
@@ -9,5 +10,6 @@ import android.util.Log;
 public class Demo {
     public void setMethod(){
         Log.e("TAG","Day03");
+        System.out.print("newDay03");
     }
 }

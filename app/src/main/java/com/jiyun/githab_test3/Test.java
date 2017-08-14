00e33deff2add69c51447c2d0a9e13009f2660b2
1397457773 <1397457773@qq.com>
2017-08-15 00:10:37 +0800

    第八次提交 代码合并
    时间：2017年8月15日00:10:33
    程序员：李俊岐

---
+++ app/src/main/java/com/jiyun/githab_test3/Test.java
@@ -0,0 +1,16 @@
+package com.jiyun.githab_test3;
+
+import android.util.Log;
+
+/**
+ * Created by Lenovo on 2017/8/14.
+ */
+
+public class Test {
+    private void newMethod(){
+        Log.e("TAG","Week02");
+        System.out.print("newWeek02");
+
+        //我也想合并代码
+    }
+}

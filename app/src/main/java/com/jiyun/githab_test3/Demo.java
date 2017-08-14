2263d934766b6db6919b8eab12b964623a1483d7
1397457773 <1397457773@qq.com>
2017-08-14 23:24:45 +0800

    第二次提交 框架内容填充，新建方法
    时间：2017年8月14日23:24:33
    程序员：李俊岐

---
+++ app/src/main/java/com/jiyun/githab_test3/Demo.java
@@ -0,0 +1,13 @@
+package com.jiyun.githab_test3;
+
+import android.util.Log;
+
+/**
+ * Created by Lenovo on 2017/8/14.
+ */
+
+public class Demo {
+    public void setMethod(){
+        Log.e("TAG","Day03");
+    }
+}
